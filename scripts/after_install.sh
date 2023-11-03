845700dc64de5cb4ed104abda0e53f312edd6581
AntonioBenci <antoniobencimail@gmail.com>
2023-11-03 18:47:57 +0100

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 echo 'run after_install.sh: ' >> /home/ec2-user/aws-github-pipeline/deploy.log
 
 echo 'cd /home/ec2-user/nodejs-server-cicd' >> /home/ec2-user/aws-github-pipeline/deploy.log
