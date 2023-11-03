8fb0d50d87b4d99c962950d58a129b67d6e4744d
AntonioBenci <antoniobencimail@gmail.com>
2023-11-03 18:45:54 +0100

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,7 +2,7 @@
 echo 'run after_install.sh: ' >> /home/ec2-user/aws-github-pipeline/deploy.log
 
 echo 'cd /home/ec2-user/nodejs-server-cicd' >> /home/ec2-user/aws-github-pipeline/deploy.log
-cd /home/ec2-user/nodejs-aws-codedeploy-pipeline >> /home/ec2-user/aws-github-pipeline/deploy.log
+cd /home/ec2-user/aws-github-pipeline >> /home/ec2-user/aws-github-pipeline/deploy.log
 
 echo 'npm install' >> /home/ec2-user/aws-github-pipeline/deploy.log 
 npm install >> /home/ec2-user/aws-github-pipeline/deploy.log
