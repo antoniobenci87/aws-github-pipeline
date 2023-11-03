646f0aac2aee235a6ea5851c890110e6ab1c3cc7
AntonioBenci <antoniobencimail@gmail.com>
2023-11-03 18:49:24 +0100

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 echo 'run application_start.sh: ' >> /home/ec2-user/aws-github-pipeline/deploy.log
 
 echo 'pm2 restart nodejs-express-app' >> /home/ec2-user/aws-github-pipeline/deploy.log
